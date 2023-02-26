aaba72314276dd517f9cf99371153de8d39b6b93
duy-tnb <duy-tnb@genkisystem.com>
2023-02-27 00:52:54 +0900

    fix bug

--- run_bot.py
+++ run_bot.py
@@ -11,7 +11,7 @@ import discord
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot, Context
 
-import discord_bot.exceptions
+import discord_bot.exceptions as exceptions
 from dotenv import load_dotenv
 
 load_dotenv('.env.local')
@@ -56,13 +56,12 @@ intents.presences = True
 """
 
 intents = discord.Intents.default()
-# intents.message_content = True
+intents.message_content = True
 """
 Uncomment this if you want to use prefix (normal) commands.
 It is recommended to use slash commands and therefore not use prefix commands.
 If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
-"""
-# intents.message_content = True
+""" 
 
 bot = Bot(
     command_prefix=commands.when_mentioned_or(config["prefix"]),
